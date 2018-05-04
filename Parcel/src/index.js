0d705ab86edcf27d4fff326b5153cfd356377192
AMAR BENSABER Hichem <hichemamb@MacBook-Pro-de-AMAR.local>
2018-05-04 11:35:29 -0700

    first commit🐒

--- Parcel/src/index.js
+++ Parcel/src/index.js
@@ -1,2 +1,23 @@
 import 'flexboxgrid';
 import './style/style.scss';
+
+
+var carre = document.querySelector('.cross');
+var carre2 = document.querySelector('.modalCross');
+var modal = document.querySelector('.modal');
+
+
+carre.addEventListener('click',function(){
+  modal.style.display='block';
+  modal.style.animationName='slideNav';
+});
+
+
+carre2.addEventListener('click',function(){
+
+
+  modal.style.animationName='slideNav2';
+  setTimeout(function(){ modal.style.display='none';}, 1000);
+
+
+});
